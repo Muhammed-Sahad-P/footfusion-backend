639f629ea8201355ce89e70443da79efb853a7bc
Muhammed Sahad P <zanusahad@gmail.com>
2024-08-19 10:04:55 +0530

    create express app and connect mongodb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.error(error));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
