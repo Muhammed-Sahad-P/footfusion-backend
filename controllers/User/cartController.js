7cf32b222d4b57cff2bb99f9b7ec59a788efb61a
Muhammed Sahad P <zanusahad@gmail.com>
2024-08-21 14:31:30 +0530

    set cartController

--- controllers/User/cartController.js
+++ controllers/User/cartController.js
@@ -1,10 +1,10 @@
 const Cart = require("../../models/cartSchema");
 const CustomError = require("../../utils/customError");
 
+// add to cart
 const addToCart = async (req, res, next) => {
   try {
-    const { productId, quantity } = req.body;
-    const userId = req.user._id;
+    const { productId, quantity, userId } = req.body;
 
     const cart = await Cart.findOne({ userId });
     if (!cart) {
@@ -33,4 +33,85 @@ const addToCart = async (req, res, next) => {
   }
 };
 
-module.exports = { addToCart };
+// get cart
+const getCart = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return next(new CustomError("Cart not found", 404));
+    }
+    return res.status(200).json(cart);
+  } catch (error) {
+    return next(new CustomError(error, 500));
+  }
+};
+
+//update cart
+const updateCart = async (req, res, next) => {
+  try {
+    const { productId, userId } = req.params;
+    const { quantity } = req.body;
+
+    if (quantity < 1) {
+      return next(new CustomError("Quantity must be greater than 1", 400));
+    }
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return next(new CustomError("Cart not found", 404));
+    }
+
+    const product = cart.products.find(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (!product) {
+      return next(new CustomError("Product not found in cart", 404));
+    }
+
+    product.quantity = quantity;
+
+    await cart.save();
+    return res.status(200).json({ message: "Cart updated successfully", cart });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Something went wrong" });
+  }
+};
+
+// delete cart
+const removeCart = async (req, res, next) => {
+  try {
+    const { productId, userId } = req.params;
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return next(new CustomError("Cart not found", 404));
+    }
+    const productExists = cart.products.some(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (!productExists) {
+      return next(new CustomError("Product not found in cart", 404));
+    }
+
+    cart.products = cart.products.filter(
+      (item) => item.productId.toString() !== productId
+    );
+
+    if (cart.products.length === 0) {
+      await Cart.deleteOne({ _id: cart._id });
+      return res.status(200).json({ message: "Cart item deleted successfully" });
+    }
+    await cart.save();
+    return res
+      .status(200)
+      .json({ message: "Product removed successfully", cart });
+  } catch (error) {
+    return next(new CustomError(error, 500));
+  }
+};
+
+module.exports = { addToCart, getCart, updateCart, removeCart };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,20 @@ const tryCatch = require("../utils/tryCatch");
 const tokenVerification = require("../middlewares/tokenVerification");
 const {getUser} = require("../controllers/User/userController");
 const productController= require("../controllers/User/productController");
+const cartController = require("../controllers/User/cartController");
 
+//user routes
 router.get("/user", tokenVerification, tryCatch(getUser));//get userown data
 
+//product routes
 router.get("/products", tokenVerification, tryCatch(productController.getAllProducts));//get all products
 router.get("/product/:productId", tokenVerification, tryCatch(productController.getProductById));//get single product
 router.get("/products/:category", tokenVerification, tryCatch(productController.getProductsByCategory));//get products by category
+
+//cart routes
+router.post("/cart", tokenVerification, tryCatch(cartController.addToCart));//add to cart
+router.get("/cart/:userId", tokenVerification, tryCatch(cartController.getCart));//get cart
+router.put("/cart/:userId/:productId", tokenVerification, tryCatch(cartController.updateCart));//update cart
+router.delete("/cart/:userId/:productId", tokenVerification, tryCatch(cartController.removeCart));//delete cart
 
 module.exports = router
