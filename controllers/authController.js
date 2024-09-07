9011638dfba01e30ef3bf5ab74f1870d3a8e4ecd
Muhammed Sahad P <zanusahad@gmail.com>
2024-09-07 17:33:28 +0530

    change cookie credential

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,7 +52,7 @@ const Login = async (req, res, next) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "Strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       });
       return res.status(200).json({ token, isAdmin: true });
